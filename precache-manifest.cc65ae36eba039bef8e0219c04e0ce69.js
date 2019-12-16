ee079f048b007ef8bb6f8f31a6b0f06136987d21
William Hall <billhall168@gmail.com>
2019-12-16 07:23:17 -0700

    Updates

---
+++ precache-manifest.cc65ae36eba039bef8e0219c04e0ce69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fb42a1415642a31170ff87e459f2972",
+    "url": "/Clickster/index.html"
+  },
+  {
+    "revision": "486306c6b2c08ba5507d",
+    "url": "/Clickster/static/css/2.d2e2750e.chunk.css"
+  },
+  {
+    "revision": "a117e42635aef7f9ceb2",
+    "url": "/Clickster/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "486306c6b2c08ba5507d",
+    "url": "/Clickster/static/js/2.3f4bdf0b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Clickster/static/js/2.3f4bdf0b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a117e42635aef7f9ceb2",
+    "url": "/Clickster/static/js/main.8e821d7a.chunk.js"
+  },
+  {
+    "revision": "f717900f4cfa236c7b35",
+    "url": "/Clickster/static/js/runtime-main.551fdd89.js"
+  }
+]);
